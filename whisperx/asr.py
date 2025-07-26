7dbd44a8d94866f7516de02530f04d39b803ad38
mavibirdesmi <mavibirdesmi@gmail.com>
2025-07-26 10:43:21 +0100

    chore: is word timestamps not enabled

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -270,6 +270,7 @@ class FasterWhisperPipeline(Pipeline):
                 percent_complete = base_progress / 2 if combined_progress else base_progress
                 print(f"Progress: {percent_complete:.2f}%...")
 
+            print(self.options.word_timestamps)
             if self.options.word_timestamps:
                 segments_as_dict = {
                     "start": vad_segments[idx]['start'],
